0fd38b59dfc006ce903f5ccd6cb132a89b83b9fd
maaaaaaaaaaa <aaa@exaple.com>
2021-09-08 15:02:42 +0900

    引数でレースを指定する関数を追加

--- nar_ShutubaTable.py
+++ nar_ShutubaTable.py
@@ -7,93 +7,173 @@ import urllib.request as req
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-"""
------------------出馬表スクレイピング部分----------------------
-"""
-
-# 指定開催場の一日のレースID取得(手動で変数を設定)
-raceNum = 202130083101  # ある日の第1レースのレースID
-race_counts = 1
-last_raceNum = 12
-
-raceNums = []
-while race_counts <= last_raceNum:
-    raceNums.append(str(raceNum))
-    raceNum += 1
-    race_counts += 1
-
-# 出馬表自動取得コード
-table = pd.DataFrame()
-browser = webdriver.Chrome(ChromeDriverManager().install())
-for raceNum in raceNums:
-    url = 'https://nar.netkeiba.com/race/shutuba.html?race_id=' + raceNum
-    browser.get(url)
-    HorseLists = browser.find_elements_by_class_name('HorseList')
-    for HorseList in HorseLists:
-        tds = HorseList.find_elements_by_tag_name('td')
-        row = []
-        for td in tds:
-            row.append(td.text)
-        table = table.append(pd.Series(row, name=raceNum))
-
-print(table)
-browser.quit()
+import re
 
 
-# table.columns
+# ---競馬場コードを格納する辞書を作るため、keyとして場名、valueとして場コードNo.を抽出する---
 
-# 不要列削除、カラム名変更
-table = table[[0, 1, 3, 6, 7, 9]]
-table_new = table.set_axis(['枠番','馬番','馬名','騎手','厩舎','オッズ'], axis='columns')
-table_new
+url = "https://nar.netkeiba.com/racecourse/racecourse_list.html?rf=sidemenu"
+response = req.urlopen(url)
+parse_html = BeautifulSoup(response, "html.parser")
 
-# table_new.dtypes
+# aタグ-textから場名、-href属性から場コード を抽出
+tags_a = parse_html.find_all('a')
+# print(tags_a)
+# print(tags_a[21:36])＊＊要改良＊＊
 
-# table_new.index
-# Index(['202130083101', '202130083101', '202130083101', '202130083101',
-#        '202130083101', '202130083101', '202130083101', '202130083101',
-#        '202130083102', '202130083102',
-#        ...
-#        '202130083112', '202130083112', '202130083112', '202130083112',
-#        '202130083112', '202130083112', '202130083112', '202130083112',
-#        '202130083112', '202130083112'],
-#       dtype='object', length=118)
+jyo_text = []
+jyo_cd = []
+for a in tags_a[21:36]:
+    text = a.text
 
+    href = a.attrs['href']
+    href = re.findall("[0-9]+", str(href))
 
-# rowName(レースID)をレースNo.に表示変更
-table_new = table_new.rename(index={
-    '202130083101':'1R', '202130083102':'2R',
-    '202130083103':'3R', '202130083104':'4R',
-    '202130083105':'5R', '202130083106':'6R',
-    '202130083107':'7R', '202130083108':'8R',
-    '202130083109':'9R', '202130083110':'10R',
-    '202130083111':'11R', '202130083112':'12R',
-})
+    jyo_text.append(text.replace('\n', ''))
+    jyo_cd.append(href)
 
-print(table_new)
-
-
-#騎手列抽出
-Jockey_lst = table_new.loc[::, '騎手']
-Jockey_lst
-
-# 重複削除
-Jockey_lst = Jockey_lst.drop_duplicates()
-Jockey_lst
+    
+#　場コードの多重リストの解消
+def flatten_2d(data):
+    for block in data:
+        for elem in block:
+            yield elem
+jyo_cd = list(flatten_2d(jyo_cd))
+# print(jyo_text)
+# print(jyo_cd)
 
 
-# 騎手名リスト作成、
-jockey_name = []
-for j in Jockey_lst:
-    jockey_name.append(j)
-# print(jockey_name)
+# jyo_cdリスト をint型に変換
+jyo_cd_int = [int(i) for i in jyo_cd]
 
-# 騎手ごとのレース情報
-for j in Jockey_lst:
-    k = table_new.query('騎手 == "{}"'.format(j))
-    print(k,'\n'*2)
 
-""""
---------------ここまで----------------
-"""
+# dict{場名: 場コード}が一旦完成
+jyo_dict = dict(zip(jyo_text, jyo_cd_int))
+# print(jyo_dict)
+
+# ----------------------------------------------------------------------------
+
+
+
+# --------------------------出馬表スクレイピング部分------------------------------
+
+# 競馬場名を入力して、jyo_cdを取得(仮)
+s_jyoCode = jyo_dict['門別競馬場']
+
+# 出馬表スクレイピングクラス(仮)
+class ShutubaTable():
+    
+    def __init__(self):
+        self.shutuba_table = pd.DataFrame()
+
+    # 出馬表スクレイピング関数(仮)、引数に"年"、"s_jyoCode"、"月"、"日"、"レースナンバー"を入力
+    def scrape_ShutubaTable(self, year, s_jyoCode, month, day, raceNum):
+        self.year = year
+        self.s_jyoCode = s_jyoCode
+        self.month = month
+        self.day = day
+        self.raceNum = raceNum
+        
+        # 引数からレースIDを作成
+        raceId = str(self.year) + str(self.s_jyoCode).zfill(2) + str(self.month).zfill(2) + str(self.day).zfill(2) + str(self.raceNum).zfill(2)
+        raceId = int(raceId)
+        
+        # 指定レースから最終レースまでのレースIDをリストに格納
+        raceId_lst = []
+        race_counts = self.raceNum
+        i = 0
+        while race_counts <= 12:
+            raceId_lst.append(str(raceId))
+            raceId += 1
+            race_counts += 1
+        # print(raceId_lst)
+
+        # 出馬表自動取得
+        self.shutuba_table = pd.DataFrame()
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        for raceId in raceId_lst:
+            url = 'https://nar.netkeiba.com/race/shutuba.html?race_id=' + str(raceId)
+            browser.get(url)
+            elements = browser.find_elements_by_class_name('HorseList')
+            for element in elements:
+                tds = element.find_elements_by_tag_name('td')
+                row = []
+                for td in tds:
+                    row.append(td.text)
+                self.shutuba_table = self.shutuba_table.append(pd.Series(row, name=raceId))
+
+        print(self.shutuba_table, '\n'*4)
+        browser.quit()
+
+# ----------------------------------------------------------------------------
+
+
+
+# ----------------------------データ成型部分(仮)---------------------------------
+
+        # table.columns
+
+        # 不要列削除、カラム名変更
+        self.shutuba_table = self.shutuba_table[[0, 1, 3, 6, 7, 9]]
+        table_new = self.shutuba_table.set_axis(['枠番','馬番','馬名','騎手','厩舎','オッズ'], axis='columns')
+        # table_new
+
+        # table_new.dtypes
+        # table_new.index
+        # Index(['202130083101', '202130083101', '202130083101', '202130083101',
+        #        '202130083101', '202130083101', '202130083101', '202130083101',
+        #        '202130083102', '202130083102',
+        #        ...
+        #        '202130083112', '202130083112', '202130083112', '202130083112',
+        #        '202130083112', '202130083112', '202130083112', '202130083112',
+        #        '202130083112', '202130083112'],
+        #       dtype='object', length=118)
+
+
+        # rowName(レースID)をレースNo.に表示変更
+        table_new = table_new.rename(index={
+            '202130090801':'1R', '202130090802':'2R',
+            '202130090803':'3R', '202130090804':'4R',
+            '202130090805':'5R', '202130090806':'6R',
+            '202130090807':'7R', '202130090808':'8R',
+            '202130090809':'9R', '202130090810':'10R',
+            '202130090811':'11R', '202130090812':'12R',
+        })
+        # print(table_new)
+
+
+        #騎手列抽出
+        Jockey_lst = table_new.loc[::, '騎手']
+        Jockey_lst
+
+        # 重複削除
+        Jockey_lst = Jockey_lst.drop_duplicates()
+        Jockey_lst
+
+        # 騎手名リスト作成、
+        jockey_name = []
+        for j in Jockey_lst:
+            jockey_name.append(j)
+        # print(jockey_name)
+
+        # 騎手ごとのレース情報
+        for j in Jockey_lst:
+            k = table_new.query('騎手 == "{}"'.format(j))
+            print(k,'\n'*2)
+
+# ---------------------------ここまでが関数内---------------------------------
+
+
+test = ShutubaTable()
+test.scrape_ShutubaTable(2021, s_jyoCode, 9, 8, 10)
+
+
+
+
+
+
+
+
+
+
+
